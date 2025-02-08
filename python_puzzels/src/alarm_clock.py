eb7e0fb31353e74637b098269b137d63bcb107cd
JamesWengUvA <james.weng@student.uva.nl>
2025-02-09 00:59:46 +0100

    Copied exercise 1 and 2

---
+++ python_puzzels/src/alarm_clock.py
@@ -0,0 +1,79 @@
+HOURS_IN_DAY = 24
+MINUTES_IN_HOUR = 60
+SECONDS_IN_MINUTE = 60
+
+
+class Time:
+    """ Represents a time of day."""
+
+    def __init__(self, hours, minutes, seconds):
+        """ Initialises a Time object with integers 'hours', 'minutes' and
+        'seconds.
+        >>> t = Time(18, 30, 0)
+        """
+        self.set_time(hours, minutes, seconds)
+
+    def __repr__(self):
+        """ Returns the string representation of a Time object.
+        >>> print( Time(8,5,30) )
+        08:05:30
+        """
+        return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
+
+    def set_time(self, hours, minutes, seconds):
+        """ Sets the time of the Time object to 'hours', 'minutes',
+        and 'seconds' making sure the values are in valid range:
+          hours:   [0, HOURS_IN_DAY)
+          minutes: [0, MINUTES_IN_HOUR)
+          seconds: [0, SECONDS_IN_MINUTE)
+        >>> time = Time(0, 0, 0)
+        >>> time.set_time(0, 0, 90)
+        >>> print(time)
+        00:01:30
+        >>> time.set_time(0, 0, 3600)
+        >>> print(time)
+        01:00:00
+        >>> time.set_time(0, 0, -1)
+        >>> print(time)
+        23:59:59
+        >>> time.set_time(10, -121, 0)
+        >>> print(time)
+        07:59:00
+        >>> time.set_time(-50, 0, 0)
+        >>> print(time)
+        22:00:00
+        >>> print(Time(10, -120, -150)) # __init__() test
+        07:57:30
+        """
+        self.hours = divmod(hours, HOURS_IN_DAY)[1] + divmod(minutes, MINUTES_IN_HOUR)[0]
+        self.minutes = divmod(minutes, MINUTES_IN_HOUR)[1] + divmod(seconds, SECONDS_IN_MINUTE)[0]
+        self.seconds = divmod(seconds, SECONDS_IN_MINUTE)[1]
+
+    def get_hours(self):
+        """ Returns the hours of the Time object.
+        >>> Time(23,0,0).get_hours()
+        23
+        """
+        return self.hours
+
+    def get_minutes(self):
+        """ Returns the minutes of the Time object.
+        >>> Time(0,59,0).get_minutes()
+        59
+        """
+        return self.minutes
+
+    def get_seconds(self):
+        """ Returns the seconds of the Time object.
+        >>> Time(0,0,59).get_seconds()
+        59
+        """
+        return self.seconds
+
+
+def main():
+    time = Time(11, 21, 21)
+    print(time.get_hours())
+
+if __name__ == "__main__":
+    main()
