25439e43287b7e210b88a6127af88c7c5eceff9a
JamesWengUvA <james.weng@student.uva.nl>
2025-02-09 15:23:01 +0100

    added event class

--- python_puzzels/src/alarm_clock.py
+++ python_puzzels/src/alarm_clock.py
@@ -110,6 +110,39 @@ class Time:
                 * SECONDS_IN_MINUTE + self.seconds)
 
 
+class Event:
+    """ Represents an event that happens at a certain time."""
+
+    def __init__(self, time, description):
+        """ Initialises an Event object with a 'time' object of type Time and a
+        'description' of type str.
+        >>> event = Event(Time(18, 30, 0), "dinner")
+        """
+        self.time = time
+        self.description = description
+
+    def __repr__(self):
+        """ Returns the string representation of an Event object.
+        >>> print( Event(Time(18, 30, 0), "dinner") )
+        18:30:00 dinner
+        """
+        return f"{self.time} {self.description}"
+
+    def get_time(self):
+        """ Returns the time of an Event object.
+        >>> print( Event(Time(18, 30, 0), "dinner").get_time() )
+        18:30:00
+        """
+        return self.time
+
+    def get_description(self):
+        """ Returns the description of an Event object.
+        >>> print( Event(Time(18, 30, 0), "dinner").get_description() )
+        dinner
+        """
+        return self.description
+
+
 def get_current_hours_minutes_seconds():
     """ Returns the current (hours, minutes, seconds) as a tuple. """
     t = time.localtime()
