1fed1d3ff90c37941d6e60d84d439fa2378d42a0
JamesWengUvA <james.weng@student.uva.nl>
2025-02-09 15:30:25 +0100

    Added alarmclock class

--- python_puzzels/src/alarm_clock.py
+++ python_puzzels/src/alarm_clock.py
@@ -143,6 +143,39 @@ class Event:
         return self.description
 
 
+class AlarmClock:
+    """ Represents an alarm clock that can handle events. """
+
+    def __init__(self):
+        """ Initialises an AlarmClock object with an empty list of events.
+        >>> alarm_clock = AlarmClock()
+        """
+        self.events = list()
+
+    def add_event(self, event):
+        """ Adds an 'event' to this AlarmClock object, it doesn't return anything.
+        >>> alarm_clock = AlarmClock()
+        >>> event = Event(Time(18, 30, 0), "dinner")
+        >>> alarm_clock.add_event(event)
+        """
+        self.events.append(event)
+
+    def __repr__(self):
+        """ Returns a string representation of the AlarmClock object.
+        >>> alarm_clock = AlarmClock()
+        >>> event = Event(Time(18, 30, 0), "dinner")
+        >>> alarm_clock.add_event(event)
+        >>> s = str(alarm_clock)
+        >>> "18:30:00" in s
+        True
+        >>> "dinner" in s
+        True
+        >>> "breakfast" in s
+        False
+        """
+        return f"{self.events}"
+
+
 def get_current_hours_minutes_seconds():
     """ Returns the current (hours, minutes, seconds) as a tuple. """
     t = time.localtime()
@@ -156,6 +189,7 @@ def now():
 
 def main():
     print("test")
+
 
 if __name__ == "__main__":
     main()
