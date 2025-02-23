dd6d07953e79c6835e2a0fbe036b599d289c82b9
JamesWengUvA <james.weng@student.uva.nl>
2025-02-23 10:58:47 +0100

    Added Solver and Random solver class
    Also changed implementation of Knapsack a little

--- opdracht_knapsack/knapsack.py
+++ opdracht_knapsack/knapsack.py
@@ -3,6 +3,7 @@ Header comment
 """
 
 import random
+import copy
 
 class Resources:
     """Describes the weight and volume of an object."""
@@ -79,21 +80,27 @@ class Items:
         self.items.append(item)
 
     def remove_item(self, index):
-        """Remove the item on the given index from the list."""
-        self.items.pop(index)
+        """Remove and return the item on the given index from the list."""
+        if self.items:
+            return self.items.pop(index)
+        return False
 
 
 class Knapsack:
     """Knapsack with items"""
-    def __init__(self, resources):
-        """Initialise an empty knapsack with 0 points and the resources."""
+    def __init__(self, capacity):
+        """Initialise an empty knapsack with 0 points and the capacity of the
+        resources."""
         self._points = 0
         self._items = Items()
-        self._resources = resources
+        self._capacity = capacity
+        self._resources = Resources(0, 0)
 
     def __repr__(self):
         """Return a string with the points, resources, and list of items."""
-        return f"Points: {self._points}; {self._resources}; Items: {self._items}"
+        return f"""Points: {self._points}; Resources: {
+               self._resources}; Capacity: {self._capacity}; Items: {
+               self._items}"""
 
     def __len__(self):
         """Return the length of the list of items in the knapsack."""
@@ -107,7 +114,9 @@ class Knapsack:
         """Return true if the item fits in the knapsack, otherwise return
         false."""
         item_resources = item.get_resources()
-        return True if item_resources.fits_in(self._resources) else False
+        if item_resources.fits_in(self._capacity - self._resources):
+            return True
+        return False
 
     def add_item(self, item):
         """Add item to knapsack and return true if the item fits in the
@@ -116,24 +125,36 @@ class Knapsack:
             return False
         self._items.add_item(item)
         self._points += item.get_points()
-        self._resources -= item.get_resources()
+        self._resources += item.get_resources()
         return True
 
     def remove_last_item(self):
-        """Remove the last item from the knapsack."""
+        """Remove and return the last item from the knapsack."""
         i = len(self) - 1
         self._points -= self._items[i].get_points()
-        self._items.remove_item(i)
+        self._resources -= self._items[i].get_resources()
+        return self._items.remove_item(i)
 
     def remove_random_item(self):
-        """Remove a random item from the knapsack."""
+        """Remove and return a random item from the knapsack."""
         i = random.randint(0, len(self) - 1)
         self._points -= self._items[i].get_points()
-        self._items.remove_item(i)
+        self._resources -= self._items[i].get_resources()
+        return self._items.remove_item(i)
 
     def get_points(self):
         """Return the total number of points in the knapsack."""
         return self._points
+
+    def get_capacity(self):
+        """Return the capacity of the knapsack."""
+        return self._capacity
+
+    def reset(self):
+        """Remove every item in the knapsack and reset the points."""
+        self._points = 0
+        self._items = Items()
+        self._resources = Resources(0, 0)
 
     def save(self, filename):
         """Save the current state of the knapsack to filename."""
@@ -143,17 +164,62 @@ class Knapsack:
                 f.write(f"{item.get_name()}\n")
 
 
+class Solver:
+    """Solver used to solve knapsack problem."""
+    def solve(self, knapsack, items):
+        """Solve the knapsack problem with the knapsack and the list of
+        items by storing the knapsack with the most points."""
+        pass
+
+    def get_best_knapsack(self):
+        """Return knapsack with the most points."""
+        return self.best_knapsack
+
+    def update_best_knapsack(self, knapsack):
+        """Update best knapsack to new knapsack."""
+        self.best_knapsack = Knapsack(knapsack.get_capacity())
+        for item in knapsack:
+            self.best_knapsack.add_item(item)
+
+
+class Solver_Random(Solver):
+    """Random solver that solves the knapsack problem by inserting random
+    items in the knapsack."""
+    def __init__(self, runs):
+        """Initialise random solver. The number of runs determine how
+        many times the solver will try to solve the knapsack problem."""
+        self.runs = runs
+        self.best_knapsack = Knapsack(Resources(0, 0))
+
+    def solve(self, knapsack, items):
+        """Solve the knapsack problem by repeatedly inserting random items into
+        the knapsack and saving the best knapsack."""
+        for run in range(self.runs):
+            knapsack.reset()
+            available_items = list(range(len(items)))
+            random.shuffle(available_items)
+            while available_items:
+                i = available_items.pop()
+                if not knapsack.add_item(items[i]):
+                    break
+
+            if self.best_knapsack.get_points() < knapsack.get_points():
+                self.update_best_knapsack(knapsack)
+
+
 def load_knapsack(knapsack_file):
     """Return a knapsack and a list of items from the knapsack_file"""
     with open(knapsack_file, "r") as f:
         lines = f.read().splitlines()
     knapsack_info = lines[1].split(", ")
-    knapsack = Knapsack(Resources(knapsack_info[2], knapsack_info[3]))
+    knapsack = Knapsack(Resources(int(knapsack_info[2]),
+                                  int(knapsack_info[3])))
     items = Items()
     for i in range(2, len(lines)):
         item_info = lines[i].split(", ")
-        items.add_item(Item(item_info[0], item_info[1],
-                                Resources(item_info[2], item_info[3])))
+        items.add_item(Item(item_info[0],
+                            int(item_info[1]),
+                            Resources(int(item_info[2]), int(item_info[3]))))
     return knapsack, items
 
 
@@ -170,9 +236,21 @@ def solve(solver, knapsack_file, solution_file):
 
 
 def test():
-    knapsack, items = load_knapsack("knapsack_small.csv")
-    print (knapsack)
-    print (items)
+    solver_random = Solver_Random(1000)
+    knapsack_file = "knapsack_small"
+    print("=== solving:", knapsack_file)
+    solve(solver_random, knapsack_file + ".csv",
+          knapsack_file + "_solution_random.csv")
+
+    knapsack_file = "knapsack_medium"
+    print("=== solving:", knapsack_file)
+    solve(solver_random, knapsack_file + ".csv",
+          knapsack_file + "_solution_random.csv")
+
+    knapsack_file = "knapsack_large"
+    print("=== solving:", knapsack_file)
+    solve(solver_random, knapsack_file + ".csv",
+          knapsack_file + "_solution_random.csv")
 
 
 def main():
