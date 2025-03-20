2d4c589f9fdfcf983ce23827c2055f48719d37ec
JamesWengUvA <james.weng@student.uva.nl>
2025-03-20 21:56:36 +0100

    Added more functions

--- Erlang/3_dots_and_boxes/grid.erl
+++ Erlang/3_dots_and_boxes/grid.erl
@@ -1,5 +1,6 @@
 -module(grid).
--export([show_hlines/2, show_vlines/2, print/1, new/2, get_wall/3, has_wall/2, add_wall/2]).
+-export([show_hlines/2, show_vlines/2, print/1, new/2, get_wall/3, has_wall/2, add_wall/2,
+    get_cell_walls/2, get_all_walls/2, get_open_spots/1, choose_random_wall/1]).
 
 new(Width, Height) ->
     {Width, Height, []}.
@@ -56,3 +57,19 @@ print(Grid) ->
     end, lists:seq(0, H)),
     io:fwrite("~n"),
     ok.
+
+get_cell_walls(X, Y) ->
+    [get_wall(X, Y, Dir) || Dir <- [north, east, south, west]].
+
+get_all_walls(W, H) ->
+    [get_wall(X, Y, north) || X <- lists:seq(0, W-1), Y <- lists:seq(0, H)]
+    ++ [get_wall(X, Y, west) || X <- lists:seq(0, W), Y <- lists:seq(0, H-1)].
+
+get_open_spots(Grid) ->
+    {W, H, Walls} = Grid,
+    lists:subtract(get_all_walls(W, H), Walls).
+
+choose_random_wall(Grid) -> case get_open_spots(Grid) of
+    [] -> [];
+    L -> lists:nth(rand:uniform(length(L)), L)
+end.
