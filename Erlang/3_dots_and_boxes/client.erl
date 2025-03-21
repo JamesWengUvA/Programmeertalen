636f75755f8a7ab1fd228d3e1f0fd66ab06200ad
JamesWengUvA <james.weng@student.uva.nl>
2025-03-21 18:12:51 +0100

    Added client and game_server logic

--- Erlang/3_dots_and_boxes/client.erl
+++ Erlang/3_dots_and_boxes/client.erl
@@ -1,16 +1,16 @@
 -module(client).
 -export([move/0, new/0]).
 
-
 move() ->
     <<S1:32, S2:32, S3:32>> = crypto:strong_rand_bytes(12),
-    rand:seed(exs1024,{S1, S2, S3}),
+    rand:seed(exsss,{S1, S2, S3}),
     receive
         finished ->
-            io:format("~p: I am done~n", [self()])
-        %TODO
+            io:format("~p: I am done~n", [self()]);
+        {move, ServerPid, Grid} ->
+            gen_server:call(ServerPid, {move, grid:choose_random_wall(Grid)}),
+            move()
     end.
-
 
 new() ->
     spawn(client, move, []).
--- Erlang/3_dots_and_boxes/game_server.erl
+++ Erlang/3_dots_and_boxes/game_server.erl
@@ -2,7 +2,7 @@
 
 -behaviour(gen_server).
 
--export([start_link/1, handle_call/3, handle_cast/2]).
+-export([start_link/1, handle_call/3, handle_cast/2, handle_continue/2]).
 -export([init/1, move/2]).
 
 start_link({W, H, Players}) ->
@@ -12,19 +12,46 @@ start_link({W, H, Players}) ->
 move(Pid, Wall) ->
     gen_server:call(Pid, {move, Wall}).
 
-
 % TODO: You need to inform the first player to move.
 init({Width, Height, Players}) ->
     Grid = grid:new(Width, Height),
+    [H|_] = Players,
+    H ! {move, self(), Grid},
     {ok, {Grid, Players}}.
 
 % TODO: add handle_call for move.
+handle_call({move, Wall}, _From, State) ->
+    {Grid, [H|T]} = State,
+    case grid:has_wall(Wall, Grid) of
+        true -> {reply, {ok, 0}, {Grid, T ++ [H]}};
+        false ->
+            New_grid = grid:add_wall(Wall, Grid),
+            {{X, Y}, {A, B}} = Wall,
+            {_, _, Walls} = New_grid,
+            case {grid:get_cell_walls(X, Y) -- Walls, grid:get_cell_walls(A, B) -- Walls} of
+                {[], []} -> {reply, {ok, 2}, {New_grid, [H|T]}, {continue, check_grid}};
+                {_, []} -> {reply, {ok, 1}, {New_grid, [H|T]}, {continue, check_grid}};
+                {[], _} -> {reply, {ok, 1}, {New_grid, [H|T]}, {continue, check_grid}};
+                {_, _} -> {reply, {ok, 0}, {New_grid, T ++ [H]}, {continue, check_grid}}
+            end
+    end;
 
 % Used for testing.
 handle_call(state, _From, State) ->
     {reply, {ok, State}, State};
 handle_call({setWalls, Walls}, _From, {{W, H, _}, Players}) ->
     {reply, ok, {{W, H, Walls}, Players}}.
+
+handle_continue(check_grid, State) ->
+    {Grid, [H|T]} = State,
+    case grid:get_open_spots(Grid) of
+        [] ->
+            lists:map(fun(X) -> X ! finished end, [H|T]),
+            {stop, finished, State};
+        _ ->
+            H ! {move, self(), Grid},
+            {noreply, State}
+    end.
 
 % Required for gen_server behaviour.
 % Normally you would implement this too,
